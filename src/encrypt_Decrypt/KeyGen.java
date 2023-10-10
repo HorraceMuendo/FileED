1aa3b937f6b063e9804b8b0cacf575aa61461e27
HorraceMuendo <horracemuendo34@gmail.com>
2023-10-10 15:42:48 +0300

    Added an init vector

--- src/encrypt_Decrypt/KeyGen.java
+++ src/encrypt_Decrypt/KeyGen.java
@@ -2,7 +2,9 @@ package encrypt_Decrypt;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class KeyGen {
     // Gen the key from a cryptographivally secure Random num gen
@@ -13,4 +15,13 @@ public class KeyGen {
         SecretKey key = keyGenerator.generateKey();
         return key;
     }
+
+
+    // create an init vector (IV)
+    public static IvParameterSpec generate(){
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
 }
+
